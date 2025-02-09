85f7ba8ea11dde4d47370751d6a3a85ad635ba18
AvinashD31 <avi31032002@gmail.com>
2025-02-09 15:37:34 +0530

    """

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-    origin: ['process.env.VITE_BASE_URL', 'http://localhost:5173'],
+    origin: [process.env.VITE_BASE_URL, 'http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
